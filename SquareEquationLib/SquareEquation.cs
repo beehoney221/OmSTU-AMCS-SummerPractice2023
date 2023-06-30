fccb164048481603a37fe4cbb0f803d606ab7c9a
mary <mashatyu@mail.ru>
2023-06-30 00:05:07 +0000

    eps = 1e-9

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,27 +4,27 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Double.Epsilon;
+        double eps = 1e-5;
         if (Math.Abs(a) <= eps) throw new System.ArgumentException();
-        else if (double.IsNan(a) | double.IsNan(b) | double.IsNan(c)) throw new ArgumentException();
+        else if (double.IsNaN(a) | double.IsNaN(b) | double.IsNaN(c)) throw new ArgumentException();
         else if (double.IsPositiveInfinity(a) | double.IsPositiveInfinity(b) | double.IsPositiveInfinity(c)) throw new ArgumentException();
         else if (double.IsNegativeInfinity(a) | double.IsNegativeInfinity(b) | double.IsNegativeInfinity(c)) throw new ArgumentException();
         else 
         {
             double d = b * b - (4 * a * c);
             double x1, x2;
-            if (Math.Abs(D) < eps) 
-            {   
-                x1 = - (b + Math.Sign(b) * Math.Sqrt(D)) / 2;
-                double[] array = { x1 };
-                return array;
-            }
-            else if (D >= eps)
+            if (d >= eps)
             {
-                if (b != 0) x1 = - (b + Math.Sign(b) * Math.Sqrt(D)) / 2;
-                else x1 = - Math.Sqrt(D) / 2;
+                if (b != 0) x1 = - (b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+                else x1 = - Math.Sqrt(d) / 2;
                 x2 = c / x1;
                 double[] array = { x1, x2 };
+                return array;
+            }            
+            else if (Math.Abs(d) < eps) 
+            {   
+                x1 = - (b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+                double[] array = { x1 };
                 return array;
             }
             else
@@ -33,8 +33,5 @@ public class SquareEquation
                 return array;
             }
         }
-        
-
-        throw new NotImplementedException();
     }
 }
