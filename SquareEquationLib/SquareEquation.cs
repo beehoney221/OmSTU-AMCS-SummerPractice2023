b09b0af87d1316fca982689dcfb9c295809a1e96
mary <mashatyu@mail.ru>
2023-06-30 07:30:53 +0000

    dop

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -5,10 +5,14 @@ public class SquareEquation
     public static double[] Solve(double a, double b, double c)
     {
         double eps = 1e-9;
-        if (Math.Abs(a) <= eps) throw new System.ArgumentException();
-        else if (double.IsNaN(a) | double.IsNaN(b) | double.IsNaN(c)) throw new ArgumentException();
-        else if (double.IsPositiveInfinity(a) | double.IsPositiveInfinity(b) | double.IsPositiveInfinity(c)) throw new ArgumentException();
-        else if (double.IsNegativeInfinity(a) | double.IsNegativeInfinity(b) | double.IsNegativeInfinity(c)) throw new ArgumentException();
+        if (Math.Abs(a) <= eps) 
+            throw new System.ArgumentException();
+        else if (double.IsNaN(a) | double.IsNaN(b) | double.IsNaN(c)) 
+            throw new ArgumentException();
+        else if (double.IsPositiveInfinity(a) | double.IsPositiveInfinity(b) | double.IsPositiveInfinity(c)) 
+            throw new ArgumentException();
+        else if (double.IsNegativeInfinity(a) | double.IsNegativeInfinity(b) | double.IsNegativeInfinity(c)) 
+            throw new ArgumentException();
         else 
         {
             double d = b * b - (4 * a * c);
