e51ef28c388a94a5b68093e4ccd4e6224b86e1c5
mary <mashatyu@mail.ru>
2023-07-02 04:38:00 +0000

    final version!!

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -6,13 +6,21 @@ public class SquareEquation
     {
         double eps = 1e-9;
         if (Math.Abs(a) <= eps) 
+        {
             throw new System.ArgumentException();
+        }
         else if (double.IsNaN(a) | double.IsNaN(b) | double.IsNaN(c)) 
+        {
             throw new ArgumentException();
+        }
         else if (double.IsPositiveInfinity(a) | double.IsPositiveInfinity(b) | double.IsPositiveInfinity(c)) 
+        {
             throw new ArgumentException();
+        }
         else if (double.IsNegativeInfinity(a) | double.IsNegativeInfinity(b) | double.IsNegativeInfinity(c)) 
+        {
             throw new ArgumentException();
+        }
         else 
         {
             double d = b * b - (4 * a * c);
