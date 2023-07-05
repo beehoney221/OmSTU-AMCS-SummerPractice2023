77ebc4bddad5f7febdf39ef8fcd35c1a155d6416
mary <mashatyu@mail.ru>
2023-07-05 14:45:04 +0000

    done!

---
+++ SpaceBattle/SpaceBattle.cs
@@ -0,0 +1,30 @@
+﻿namespace SpaceBattle;
+public class SpaceBattle
+{
+    public double[] pos = { double.NaN, double.NaN };
+    public double[] speed = { double.NaN, double.NaN };
+    public static double[] Position(double[] pos, double[] speed) 
+    {
+        if ((pos.Length == 0) | (speed.Length == 0))
+            throw new Exception();
+        else
+        {
+            double[] array = { pos[0] + speed[0], pos[1] + speed[1] };
+            return array;
+        }
+    } 
+
+    public static double[] GiveS(double a, double b)
+    {
+        if ((a == double.NaN) | (b == double.NaN)) 
+        {
+            double[] array = { };
+            return array;
+        }
+        else
+        {
+            double[] array = { Convert.ToDouble(a), Convert.ToDouble(b) };
+            return array;
+        }
+    }
+}
